5aa2c92a33f650fc43f9bd919b32c2af729edc33
Sean O'Brien <msobrien90@gmail.com>
2018-01-31 19:20:48 -0700

    More descriptive comments about the output file.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Open (input) MAL program file and log (output) file
+            // Create the log file in the same location
 
         // For each MAL program line...
             // Read line
@@ -14,24 +15,24 @@ public class Main {
                 // Write line to log file, with sequence number and without any online comment
 
             // Check errors (in order below), stopping at first error on that line
-                    // 1) Ill-formed label: first token ends in a colon but doesn't follow rules for a label name
+                // 1) Ill-formed label: first token ends in a colon but doesn't follow rules for a label name
 
-                    // 2) Invalid opcode: first non-comment string (token) found on a line is not one of the valid opcodes
-                        // Exception: if line contains a label, then opcode should be second token
+                // 2) Invalid opcode: first non-comment string (token) found on a line is not one of the valid opcodes
+                    // Exception: if line contains a label, then opcode should be second token
 
-                    // 3) Too many operands: for the specific opcode, there are more operands than expected
-                        // You may assume that any operands on the line are in a valid, comma-seperated list for this
-                        // the next error
+                // 3) Too many operands: for the specific opcode, there are more operands than expected
+                    // You may assume that any operands on the line are in a valid, comma-seperated list for this
+                    // the next error
 
-                    // 4) Too few opcode: for the specific opcode, there are fewer operands than expected
+                // 4) Too few opcode: for the specific opcode, there are fewer operands than expected
 
-                    // 5) Ill-formed operands: an operand name (id or register) is invalid, or an immediate value is
-                    // for octal
+                // 5) Ill-formed operands: an operand name (id or register) is invalid, or an immediate value is
+                // for octal
 
-                    // 6) Wrong operand type: Number where id expected, or vice versa
+                // 6) Wrong operand type: Number where id expected, or vice versa
 
-                    // 7) Label warnings: a branch goes to a non-existent label, or there is a label that is never
-                    // branched to
+                // 7) Label warnings: a branch goes to a non-existent label, or there is a label that is never
+                // branched to
 
         // Detection of an error on a line halts processing on that line (don't scan for further errors)
 
